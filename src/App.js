d6cf990a4b09a201d8a1909e635fb5ac4d223758
camilalopes <kmilalopes10@gmail.com>
2019-03-03 20:23:54 -0300

    add the map component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="map" role="map">
+        <Map
+          google={this.props.google}
+          zoom={14}
+          initialCenter={{
+           lat: -1.2884,
+           lng: 36.8233
+          }}
+        />
       </div>
     );
   }
 }
 
-export default App;
+//export default App;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyASTD9r6pWJjIKgNUaQ46sWcMnkF2Nh-T4'
+})(App);
