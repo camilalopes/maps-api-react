09ff3da611e677fa55cdc6462f52cd8db82954b5
camilalopes <kmilalopes10@gmail.com>
2019-03-03 22:10:22 -0300

    creating a list of active markers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    showingInfoWindow: false,  //Hides or the shows the infoWindow
+    activeMarkers: [
+      {
+        position: {lat: -1.2884, lng: 36.8233},
+        name: 'Restaurante'
+      },
+      {
+        position: {lat: -1.28, lng: 36.8233},
+        name: 'Padaria'
+      }
+    ],
+    selectedPlace: {}          //Shows the infoWindow to the selected place upon a marker
+  };
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+  });
+
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+
   render() {
     return (
-      <div className="map" role="map">
+      <div className="map">
         <Map
           google={this.props.google}
           zoom={14}
@@ -13,13 +45,28 @@ class App extends Component {
            lat: -1.2884,
            lng: 36.8233
           }}
-        />
+        >
+
+          {this.state.activeMarkers.map((marker) => {
+            return <Marker onClick={this.onMarkerClick}
+              position={marker.position} name={marker.name} />
+          })}
+
+          <InfoWindow
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}
+            onClose={this.onClose}
+          >
+            <div>
+              <h4>{this.state.selectedPlace.name}</h4>
+            </div>
+          </InfoWindow>
+        </Map>
       </div>
     );
   }
 }
 
-//export default App;
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyASTD9r6pWJjIKgNUaQ46sWcMnkF2Nh-T4'
 })(App);
